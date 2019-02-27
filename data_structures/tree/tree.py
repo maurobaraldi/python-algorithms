0f55aeaa552057650a46f943073a490ee26e75ef
Mauro Navarro Baraldi <mauro.baraldi@gmail.com>
2019-02-27 19:56:06 -0300

    Improve identation and fix comments.

--- data_structures/tree/tree.py
+++ data_structures/tree/tree.py
@@ -23,18 +23,13 @@ class BinarySearchTree:
 
         if self.root is None:
             self.root = Node(value)
-
         else:
-
             if value <= node.value:  # Go to branch / of tree.
-
                 if node.left is not None:  # This node, already has a node.
                     self.add(value, node.left)  # Re-call add new node mehtod.
                 else:
                     node.left = Node(value)  # node is None, add new node.
-
             else:
-
                 if node.right is not None:
                     self.add(value, node.right)
                 else:
@@ -42,23 +37,15 @@ class BinarySearchTree:
 
     def search(self, value, node=None):
         """Search for node containing value (recursively)."""
-        if node is None:
+        if node is None or self.root.value == value:
             node = self.root
-
-        if self.root.value == value:
-            return self.root
-
         else:
-
             if node.value == value:
                 return node
-
             if node.value > value and node.left is not None:
                 return self.search(value, node.left)
-
             if node.value < value and node.right is not None:
                 return self.search(value, node.right)
-
             return None
 
     def min(self, node):
@@ -95,13 +82,10 @@ class BinarySearchTree:
         # Case 1
         if value < node.value:
             node.left = self.delete(node.left, value)
-
         # Case 2
         elif value > node.value:
             node.right = self.delete(node.right, value) 
-
         else:
-
             # Node with only one child or no child
             if node.left is None:
                 n = node.right
@@ -111,7 +95,6 @@ class BinarySearchTree:
                 n = node.left
                 node = None
                 return n
-
             # Node with two children: Get the inorder successor (smallerst in the right subtree).
             n = self.min(node.right)
             node.value = n.value  # Copy the inorder successor's content to this node.
@@ -132,14 +115,13 @@ class BinarySearchTree:
 
         if node is not None:
             print(node.value),
-            self.pre_order(node.left)  # Recursive in_order.
+            self.pre_order(node.left)  # Recursive pre_order.
             self.pre_order(node.right)
 
     def post_order(self, node):
         """Traverse (in_order) tree and print each item."""
 
         if node is not None:
-            self.post_order(node.left)  # Recursive in_order.
+            self.post_order(node.left)  # Recursive post_order.
             self.post_order(node.right)
             print(node.value),
-
